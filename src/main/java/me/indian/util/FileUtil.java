3240f3424ded6896b2ac69672b57afc5c03b93e0
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-09-21 15:14:35 +0200

    Update file extensions info

--- src/main/java/me/indian/util/FileUtil.java
+++ src/main/java/me/indian/util/FileUtil.java
@@ -115,45 +115,146 @@ public final class FileUtil {
         return attrs.creationTime().toMillis();
     }
 
-    //TODO: Zaktualizowac to
-    public static String getFileExtensionInfo(final String fileName) {
+    public static String getFileType(final String fileName) {
         final int dotIndex = fileName.lastIndexOf('.');
 
         final String fileExtension = ((dotIndex > 0) ? fileName.substring(dotIndex + 1) : "Unknown");
         return switch (fileExtension.toLowerCase()) {
-            case "exe" -> "Aplikacja";
-            case "md" -> "MarkDown";
+            // Executable Files
+            case "exe" -> "Windows Executable";
+            case "msi" -> "Windows Installer Package";
+            case "scr" -> "Windows Screen Saver Executable";
+            case "out" -> "Java Executable";
+            case "jar" -> "Java Architecture File";
+            case "app" -> "macOS Executable";
+            case "elf" -> "Linux Executable";
+            case "deb" -> "Debian Package";
+            case "rpm" -> "RedHat Package";
+            case "apk" -> "Android Application Package";
+
+            // Markdown
+            case "md", "markdown", "mdown", "mkd", "mkdn", "mdtxt" -> "Markdown File";
+
+            // Text Files
             case "txt" -> "Text File";
+            case "log" -> "Log File";
+            case "rtf" -> "Rich Text Format file";
+
+            //Minecraft
+            case "mcaddon" -> "Minecraft Addon";
+            case "mcpack" -> "Minecraft Resource/Behavior Pack";
+            case "mcmeta" -> "Minecraft Resource Pack Configuration File.";
+            case "mcproject" -> "Minecraft Editor File";
+            case "mcstructure" -> "Minecraft Structure File";
+            case "mctemplate" -> "Minecraft World Template";
+            case "mcworld" -> "Minecraft World";
+
+            // Data Files
             case "json" -> "Json File";
+            case "csv" -> "CSV File";
+            case "sql" -> "SQL File";
+            case "xml" -> "XML File";
+            case "xps" -> "XML-based document";
+            case "dat", "data" -> "Data File";
+            case "ldb" -> "LevelDB File";
+            case "tmp" -> "Temporary data file";
+
+            // Images
             case "jpg", "jpeg" -> "JPEG Image";
             case "png" -> "PNG Image";
             case "gif" -> "GIF Image";
             case "svg" -> "SVG Image";
+            case "webp" -> "WebP Image";
+            case "heic" -> "HEIC Image";
+            case "avif" -> "AVIF Image";
+
+            // Video Files
             case "mp4" -> "MP4 Video";
             case "avi" -> "AVI Video";
             case "mkv" -> "MKV Video";
+            case "mov" -> "QuickTime Movie File";
+            case "webm" -> "WebM Video";
+
+            // Audio Files
             case "mp3" -> "MP3 Audio";
+            case "ogg" -> "OGG Audio";
             case "wav" -> "WAV Audio";
             case "flac" -> "FLAC Audio";
+            case "m4a" -> "M4A Audio";
+            case "alac" -> "Apple Lossless Audio Codec";
+
+            // Documents
             case "pdf" -> "PDF Document";
             case "docx" -> "Word Document";
             case "xlsx" -> "Excel Spreadsheet";
+            case "pptx" -> "PowerPoint Presentation";
+            case "html", "htm" -> "HTML Document";
+
+            // Archives
             case "zip" -> "ZIP Archive";
             case "rar" -> "RAR Archive";
             case "tar" -> "TAR Archive";
             case "gz" -> "GZ Compressed Archive";
-            case "csv" -> "CSV File";
-            case "sql" -> "SQL File";
-            case "xml" -> "XML File";
-            case "pptx" -> "PowerPoint Presentation";
+            case "bz2" -> "BZ2 Compressed Archive";
+            case "7z" -> "7-Zip Archive";
+
+            // Disk Images
+            case "iso" -> "ISO Disk Image";
+            case "img" -> "Disk Image";
+            case "dmg" -> "MacOS Disk Image";
+            case "bin" -> "Binary File";
+            case "cue" -> "Cue Sheet";
+            case "mdf" -> "Media Descriptor File";
+            case "nrg" -> "Nero Disk Image";
+
+            // Scripts
             case "sh" -> "Bash Script";
+            case "bash" -> "Bash Script";
+            case "bat" -> "Batch Script";
             case "ps1" -> "PowerShell Script";
+            case "gradle" -> "Gradle Build Script";
+            case "gradlew" -> "Gradle Wrapper Script";
+            case "kts" -> "Kotlin Script for Gradle";
+            case "node" -> "Node.js Script";
+            case "py" -> "Python Script";
+            case "java" -> "Java Source File";
+            case "kt" -> "Kotlin Source File";
+            case "scala" -> "Scala Source File";
+            case "groovy" -> "Groovy Script";
+            case "rb" -> "Ruby Script";
+            case "go" -> "Go Source File";
+            case "js" -> "JavaScript File";
+            case "ts" -> "TypeScript File";
+            case "php" -> "PHP Script";
+            case "css" -> "CSS File";
+            case "rs" -> "Rust Source File";
+            case "d" -> "D Source File";
+            case "clj" -> "Clojure Source File";
+            case "pl" -> "Perl Script";
+            case "r" -> "R Script";
+            case "swift" -> "Swift Source File";
+            case "dart" -> "Dart Source File";
+            case "lua" -> "Lua Script";
+            case "v" -> "V Source File";
+            case "awk" -> "AWK Script";
+            case "cpp" -> "C++ Source File";
+            case "c" -> "C Source File";
+            case "cs" -> "C# Source File";
+            case "fs" -> "F# Source File";
+            case "fsx" -> "F# Script File";
+            case "fsi" -> "F# Interface File";
+
+
+            // Configuration Files
             case "yaml", "yml" -> "YAML File";
             case "ini" -> "INI Configuration File";
-            case "bz2" -> "BZ2 Compressed Archive";
-            case "java" -> "Java Source Code";
-            case "py" -> "Python Source Code";
-            case "html" -> "HTML Document";
+            case "properties", "props" -> "Properties Configuration File";
+            case "toml" -> "TOML Configuration File";
+            case "conf", "config", "cfg" -> "Configuration File";
+            case "env" -> "Environment File";
+            case "plist" -> "macOS Property List File";
+            case "rc" -> "Resource Configuration File";
+            case "desktop" -> "Desktop Entry File";
 
             default -> fileExtension;
         };
