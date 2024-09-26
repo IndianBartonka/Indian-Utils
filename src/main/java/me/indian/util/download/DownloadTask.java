ee2f9856cd255664a6926045360ab73dae8c126b
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-09-26 13:58:10 +0200

    Update DownloadTask.java

--- src/main/java/me/indian/util/download/DownloadTask.java
+++ src/main/java/me/indian/util/download/DownloadTask.java
@@ -18,7 +18,7 @@ public class DownloadTask {
     private final BufferUtil.DownloadBuffer downloadBuffer;
     private final int timeOutSeconds;
     private final DownloadListener downloadListener;
-    private boolean canDownload, downloading, finished;
+    private boolean stopped, downloading, finished;
 
     public DownloadTask(final InputStream inputStream, final File outputFile, final long fileSize, final BufferUtil.DownloadBuffer downloadBuffer, final int timeOutSeconds, @Nullable final DownloadListener downloadListener) throws IOException, TimeoutException {
         this.inputStream = inputStream;
@@ -27,7 +27,7 @@ public class DownloadTask {
         this.downloadBuffer = downloadBuffer;
         this.timeOutSeconds = timeOutSeconds;
         this.downloadListener = downloadListener;
-        this.canDownload = true;
+        this.stopped = false;
         this.downloading = false;
     }
 
@@ -38,7 +38,7 @@ public class DownloadTask {
         this.downloadBuffer = downloadBuffer;
         this.timeOutSeconds = timeOutSeconds;
         this.downloadListener = null;
-        this.canDownload = true;
+        this.stopped = false;
         this.downloading = false;
     }
 
@@ -51,8 +51,9 @@ public class DownloadTask {
             final int definedBuffer = BufferUtil.defineBuffer(this.downloadBuffer, this.fileSize);
             final byte[] buffer = new byte[definedBuffer];
 
-            if (this.downloadListener != null)
+            if (this.downloadListener != null) {
                 this.downloadListener.onStart(this.downloadBuffer, definedBuffer, this.outputFile);
+            }
 
             int bytesRead;
             long totalBytesRead = 0;
@@ -62,12 +63,11 @@ public class DownloadTask {
             int lastProgress = -1;
 
             while ((bytesRead = this.inputStream.read(buffer)) != -1) {
-                if (!this.canDownload) break;
+                if (this.stopped) break;
 
                 lastActivityTime = System.currentTimeMillis();
                 fileOutputStream.write(buffer, 0, bytesRead);
                 totalBytesRead += bytesRead;
-
 
                 final long currentTime = System.currentTimeMillis();
                 final double elapsedTime = (currentTime - lastTime) / 1000.0;
@@ -84,14 +84,16 @@ public class DownloadTask {
                     final long remainingTimeSeconds = (long) (MathUtil.bytesToMB(this.fileSize) / formatedSpeed);
                     final String remainingTimeString = DateUtil.formatTimeDynamic(remainingTimeSeconds * 1000, true);
 
-                    if (this.downloadListener != null)
+                    if (this.downloadListener != null) {
                         this.downloadListener.onSecond(progress, formatedSpeed, remainingTimeString);
+                    }
 
                     if (progress != lastProgress) {
                         lastProgress = progress;
 
-                        if (this.downloadListener != null)
+                        if (this.downloadListener != null) {
                             this.downloadListener.onProgress(progress, formatedSpeed, remainingTimeString);
+                        }
                     }
                 }
 
@@ -101,9 +103,9 @@ public class DownloadTask {
                 }
             }
 
-            this.inputStream.close();
-
-            if (this.downloadListener != null) this.downloadListener.onEnd(this.outputFile);
+            if (this.downloadListener != null && !this.stopped) {
+                this.downloadListener.onEnd(this.outputFile);
+            }
         } finally {
             this.downloading = false;
             this.finished = true;
@@ -139,8 +141,8 @@ public class DownloadTask {
         return this.downloadListener;
     }
 
-    public boolean isCanDownload() {
-        return this.canDownload;
+    public boolean isStopped() {
+        return this.stopped;
     }
 
     public boolean isDownloading() {
@@ -153,7 +155,7 @@ public class DownloadTask {
 
     public void stopDownload() {
         if (!this.downloading) throw new IllegalStateException("Brak aktywnego pobierania.");
-        this.canDownload = false;
+        this.stopped = true;
     }
 
     @Override
@@ -163,7 +165,7 @@ public class DownloadTask {
                 ", fileSize=" + this.fileSize +
                 ", downloadBuffer=" + this.downloadBuffer +
                 ", timeOutSeconds=" + this.timeOutSeconds +
-                ", canDownload=" + this.canDownload +
+                ", stopped=" + this.stopped +
                 ", downloading=" + this.downloading +
                 ", finished=" + this.finished +
                 ')';
