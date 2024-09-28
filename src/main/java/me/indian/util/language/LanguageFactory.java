4a00e9bcb2a538120f65fbe7d98e31fa69f1e086
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-09-28 14:17:00 +0200

    Added TODO

---
+++ src/main/java/me/indian/util/language/LanguageFactory.java
@@ -0,0 +1,7 @@
+package me.indian.util.language;
+
+public class LanguageFactory {
+
+    //TODO: Zrobic jakieś language factory ktore bedzie serializowalo i desreializowalo pliki `lang` na klasy
+
+}
