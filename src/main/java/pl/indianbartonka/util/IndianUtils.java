6def22209e91a08d8d2403e36b2a7e4cff1859ed
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-10-14 00:58:28 +0200

    Geting IndianUtils version from file

--- src/main/java/pl/indianbartonka/util/IndianUtils.java
+++ src/main/java/pl/indianbartonka/util/IndianUtils.java
@@ -1,10 +1,31 @@
 package pl.indianbartonka.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public final class IndianUtils {
 
-    public static String VERSION = "0.0.7.4";
+    public static String VERSION = "UNKNOWN";
 
     private IndianUtils() {
 
+    }
+
+    static {
+        final InputStream inputStream = IndianUtils.class.getResourceAsStream("/IndianUtils.version");
+
+        if (inputStream != null) {
+            try (final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    VERSION = line;
+                }
+            } catch (final IOException ignored) {
+
+            }
+        }
     }
 }
