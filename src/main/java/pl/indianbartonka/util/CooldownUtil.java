3347e96f31eb0be325d5b1d55f02b24037458afd
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-10-21 23:15:06 +0200

    [Phone] added Docs

--- src/main/java/pl/indianbartonka/util/CooldownUtil.java
+++ src/main/java/pl/indianbartonka/util/CooldownUtil.java
@@ -1,5 +1,8 @@
 package pl.indianbartonka.util;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -13,10 +16,18 @@ public final class CooldownUtil {
      * Private constructor to prevent instantiation of the utility class.
      */
     private CooldownUtil() {
-
+        // Prevent instantiation
     }
 
-    public static boolean cooldown(final String name, final long duration, final TimeUnit timeUnit) {
+    /**
+     * Sets a cooldown for the given name with the specified duration.
+     *
+     * @param name     the name to set the cooldown for
+     * @param duration the duration of the cooldown
+     * @param timeUnit the time unit of the duration
+     * @return true if the cooldown was successfully set, false if it was already active
+     */
+    public static boolean cooldown(@NotNull final String name, final long duration, @NotNull final TimeUnit timeUnit) {
         final long time = timeUnit.toMillis(duration);
 
         COOLDOWN.put(name, System.currentTimeMillis());
@@ -25,7 +36,15 @@ public final class CooldownUtil {
         return hasCooldown(name);
     }
 
-    public static void cooldownWait(final String name, final long duration, final TimeUnit timeUnit) {
+    /**
+     * Waits for the cooldown to expire for the specified name.
+     * If there is already a cooldown, this method returns immediately.
+     *
+     * @param name     the name to set the cooldown for
+     * @param duration the duration of the cooldown
+     * @param timeUnit the time unit of the duration
+     */
+    public static void cooldownWait(@NotNull final String name, final long duration, @NotNull final TimeUnit timeUnit) {
         if (COOLDOWN.containsKey(name)) return;
 
         final long time = timeUnit.toMillis(duration);
@@ -36,11 +55,23 @@ public final class CooldownUtil {
         waitFor(name);
     }
 
-    public static long getTime(final String name) {
+    /**
+     * Gets the duration of the cooldown for the specified name.
+     *
+     * @param name the name to get the cooldown duration for
+     * @return the cooldown duration in milliseconds
+     */
+    public static long getTime(@NotNull final String name) {
         return TIME_MAP.getOrDefault(name, 0L);
     }
 
-    public static long getRemainingTime(final String name) {
+    /**
+     * Gets the remaining time for the cooldown of the specified name.
+     *
+     * @param name the name to check the remaining cooldown for
+     * @return the remaining time in milliseconds
+     */
+    public static long getRemainingTime(@NotNull final String name) {
         if (!COOLDOWN.containsKey(name)) return 0;
 
         final long startTime = COOLDOWN.get(name);
@@ -50,7 +81,12 @@ public final class CooldownUtil {
         return cooldownDuration - elapsedTime;
     }
 
-    public static void waitFor(final String name) {
+    /**
+     * Waits until the cooldown expires for the specified name.
+     *
+     * @param name the name to wait for
+     */
+    public static void waitFor(@NotNull final String name) {
         while (hasCooldown(name)) {
             final long remaining = getRemainingTime(name);
 
@@ -65,7 +101,13 @@ public final class CooldownUtil {
         TIME_MAP.remove(name);
     }
 
-    public static boolean hasCooldown(final String name) {
+    /**
+     * Checks if there is an active cooldown for the specified name.
+     *
+     * @param name the name to check for cooldown
+     * @return true if there is an active cooldown, false otherwise
+     */
+    public static boolean hasCooldown(@NotNull final String name) {
         return COOLDOWN.containsKey(name) && (System.currentTimeMillis() - COOLDOWN.get(name)) < getTime(name);
     }
 }
