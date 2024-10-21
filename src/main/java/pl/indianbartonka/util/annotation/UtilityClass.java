661a990f2b016e56b1facfe4c3e978912fd5bf5b
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-10-21 23:10:06 +0200

    [Phone] add TODO

---
+++ src/main/java/pl/indianbartonka/util/annotation/UtilityClass.java
@@ -0,0 +1,11 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE) // Adnotacja może być używana tylko na klasach
+@Retention(RetentionPolicy.CLASS) // Adnotacja dostępna w czasie kompilacji
+public @interface UtilityClass {
+}
+
+//TODO: Dodaj tą adnotacje w klasach takich jak ZipUtil.itp aby kod ładniej wygladał
