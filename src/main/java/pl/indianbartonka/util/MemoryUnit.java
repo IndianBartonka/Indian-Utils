00ca499a9c046129a5066a64760afd8b2fc60db7
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-10-22 20:15:05 +0200

    Added `MemoryUnit`

---
+++ src/main/java/pl/indianbartonka/util/MemoryUnit.java
@@ -0,0 +1,31 @@
+package pl.indianbartonka.util;
+
+/**
+ * Enum representing different memory units.
+ * <p>
+ * Documents and math operations written by ChatGPT
+ * </p>
+ */
+public enum MemoryUnit {
+
+    BYTES(1),
+    KILOBYTES(1024),
+    MEGABYTES(1024 * 1024),
+    GIGABYTES(1024 * 1024 * 1024);
+
+    private final double bytes;
+
+    MemoryUnit(final double bytes) {
+        this.bytes = bytes;
+    }
+
+    public double to(final double value, final MemoryUnit targetUnit) {
+        final double valueInBytes = value * this.bytes;
+        return valueInBytes / targetUnit.bytes;
+    }
+
+    public double from(final double value, final MemoryUnit sourceUnit) {
+        final double valueInBytes = value * sourceUnit.bytes;
+        return valueInBytes / this.bytes;
+    }
+}
