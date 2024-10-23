565f1aa2374dae9f81f88674ae8de57692374bbb
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-10-23 16:28:35 +0200

    Added docs

--- src/main/java/pl/indianbartonka/util/MemoryUnit.java
+++ src/main/java/pl/indianbartonka/util/MemoryUnit.java
@@ -19,21 +19,49 @@ public enum MemoryUnit {
         this.bytes = bytes;
     }
 
+    /**
+     * Converts a value from the current memory unit to the target memory unit.
+     *
+     * @param value      The value to be converted from the current memory unit.
+     * @param targetUnit The target memory unit to which the value will be converted.
+     * @return The converted value in the target memory unit.
+     */
     public double to(final double value, final MemoryUnit targetUnit) {
         final double valueInBytes = value * this.bytes;
         return valueInBytes / targetUnit.bytes;
     }
 
+    /**
+     * Converts a value from the specified source memory unit to the current memory unit.
+     *
+     * @param value      The value to be converted from the source memory unit.
+     * @param sourceUnit The source memory unit from which the value will be converted.
+     * @return The converted value in the current memory unit.
+     */
     public double from(final double value, final MemoryUnit sourceUnit) {
         final double valueInBytes = value * sourceUnit.bytes;
         return valueInBytes / this.bytes;
     }
 
-    public long to(final long value, final MemoryUnit unit) {
-        return (long) this.to((double) value, unit);
+    /**
+     * Converts a long value from the current memory unit to the target memory unit.
+     *
+     * @param value      The long value to be converted from the current memory unit.
+     * @param targetUnit The target memory unit to which the value will be converted.
+     * @return The converted value in the target memory unit as a long.
+     */
+    public long to(final long value, final MemoryUnit targetUnit) {
+        return (long) this.to((double) value, targetUnit);
     }
 
-    public long from(final long value, final MemoryUnit unit) {
-        return (long) this.from((double) value, unit);
+    /**
+     * Converts a long value from the specified source memory unit to the current memory unit.
+     *
+     * @param value      The long value to be converted from the source memory unit.
+     * @param sourceUnit The source memory unit from which the value will be converted.
+     * @return The converted value in the current memory unit as a long.
+     */
+    public long from(final long value, final MemoryUnit sourceUnit) {
+        return (long) this.from((double) value, sourceUnit);
     }
 }
