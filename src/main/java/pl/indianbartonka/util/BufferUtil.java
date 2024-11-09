b1abc18c31ce9fae2f33ba65324ce9cdaba9ca75
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-11-09 15:09:16 +0100

    Update BufferUtil

--- src/main/java/pl/indianbartonka/util/BufferUtil.java
+++ src/main/java/pl/indianbartonka/util/BufferUtil.java
@@ -8,6 +8,7 @@ public final class BufferUtil {
 
     private static int MIN_BUFFER = 4_09;
     private static int MAX_BUFFER = (256 * 1_048_576);
+    private static int RAM_USAGE_DIVISOR = 5;
 
     /**
      * Private constructor to prevent instantiation of the utility class.
@@ -17,7 +18,7 @@ public final class BufferUtil {
     }
 
     public static int calculateOptimalBufferSize(final long fileSize) {
-        final long bufferPerRequest = (SystemUtil.getFreeRam() + SystemUtil.getFreeSwap()) / 5;
+        final long bufferPerRequest = (SystemUtil.getFreeRam() + SystemUtil.getFreeSwap()) / RAM_USAGE_DIVISOR;
         final long bufferSize = Math.min((long) (fileSize * 0.1), bufferPerRequest);
 
         return (int) MathUtil.getCorrectNumber(bufferSize, MIN_BUFFER, MAX_BUFFER);
@@ -37,5 +38,20 @@ public final class BufferUtil {
 
     public static void setMaxBuffer(final int maxBuffer) {
         MAX_BUFFER = maxBuffer;
+    }
+
+    public static int getRamUsageDivisor() {
+        return RAM_USAGE_DIVISOR;
+    }
+
+    /**
+     * Sets the divisor for calculating RAM usage.
+     * Ensures the divisor is a positive integer greater than 0.
+     * If out of range, the divisor is clamped to the valid bounds.
+     *
+     * @param ramUsageDivisor The divisor for RAM usage calculation.
+     */
+    public static void setRamUsageDivisor(final int ramUsageDivisor) {
+        RAM_USAGE_DIVISOR = MathUtil.getCorrectNumber(ramUsageDivisor, 1, Integer.MAX_VALUE);
     }
 }
--- src/test/java/pl/indianbartonka/util/BufferUtilTest.java
+++ src/test/java/pl/indianbartonka/util/BufferUtilTest.java
@@ -17,7 +17,7 @@ public class BufferUtilTest {
     };
 
     @Test
-    public void test(){
+    public void test() {
         this.logger.info("&aFree Ram:&b " + MathUtil.formatBytesDynamic(SystemUtil.getFreeRam(), false));
         this.logger.info("&aFree SWAP:&b" + MathUtil.formatBytesDynamic(SystemUtil.getFreeSwap(), false));
         this.logger.info("&aFree Ram + SWAP:&b " + MathUtil.formatBytesDynamic(SystemUtil.getFreeRam() + SystemUtil.getFreeSwap(), false));
@@ -25,10 +25,36 @@ public class BufferUtilTest {
 
         final File[] files = new File(System.getProperty("user.dir")).listFiles();
         if (files != null) {
-            for (final File file : files) {
-                final int buffer = BufferUtil.calculateOptimalBufferSize(FileUtil.getFileSize(file));
-                this.logger.info(file.getName() + " = " + MathUtil.formatBytesDynamic(buffer, false));
+            for (int i = 1; i < 10; i++) {
+                BufferUtil.setRamUsageDivisor(i);
+
+                this.logger.println();
+                this.logger.info("&aRam divisor:&b " + i);
+                for (final File file : files) {
+                    final int buffer = BufferUtil.calculateOptimalBufferSize(FileUtil.getFileSize(file));
+                    this.logger.info(file.getName() + " = " + MathUtil.formatBytesDynamic(buffer, false));
+                }
             }
+        }
+    }
+
+    @Test
+    public void testFile() {
+        this.logger.info("&aFree Ram:&b " + MathUtil.formatBytesDynamic(SystemUtil.getFreeRam(), false));
+        this.logger.info("&aFree SWAP:&b" + MathUtil.formatBytesDynamic(SystemUtil.getFreeSwap(), false));
+        this.logger.info("&aFree Ram + SWAP:&b " + MathUtil.formatBytesDynamic(SystemUtil.getFreeRam() + SystemUtil.getFreeSwap(), false));
+        this.logger.println();
+
+        final File file = new File(System.getProperty("user.dir")).listFiles()[0];
+
+        for (int i = 1; i < 20; i++) {
+            BufferUtil.setRamUsageDivisor(i);
+
+            this.logger.println();
+            this.logger.info("&aRam divisor:&b " + BufferUtil.getRamUsageDivisor());
+
+            final int buffer = BufferUtil.calculateOptimalBufferSize(FileUtil.getFileSize(file));
+            this.logger.info(file.getName() + " = " + MathUtil.formatBytesDynamic(buffer, true));
         }
     }
 }
