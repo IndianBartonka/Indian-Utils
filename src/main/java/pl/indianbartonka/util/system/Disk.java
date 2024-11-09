50e24acc3506a6f1a5dc07278fe3a4ee91817be5
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-11-09 13:50:05 +0100

    Remove TODO

--- src/main/java/pl/indianbartonka/util/system/Disk.java
+++ src/main/java/pl/indianbartonka/util/system/Disk.java
@@ -4,16 +4,3 @@ import java.io.File;
 
 public record Disk(String name, File diskFile, String type, long blockSize, boolean readOnly) {
 }
-
-//TODO: Przetestuj:
-for (FileStore store : FileSystems.getDefault().getFileStores()) {
-            try {
-                System.out.println("Urządzenie: " + store);
-                System.out.println("Typ: " + store.type());
-                System.out.println("Wolne miejsce: " + store.getUsableSpace() / (1024 * 1024) + " MB");
-                System.out.println("Całkowita pojemność: " + store.getTotalSpace() / (1024 * 1024) + " MB");
-                System.out.println("-------");
-            } catch (Exception e) {
-                System.out.println("Błąd przy dostępie do urządzenia: " + e.getMessage());
-            }
-}
