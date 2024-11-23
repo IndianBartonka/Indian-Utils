73fe89e43f9b37f6bd3e64e19085e3f7cd262d79
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-11-23 15:22:22 +0100

    Update NetworkUtilTest.java

--- src/test/java/pl/indianbartonka/util/NetworkUtilTest.java
+++ src/test/java/pl/indianbartonka/util/NetworkUtilTest.java
@@ -1,5 +1,9 @@
 package pl.indianbartonka.util;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pl.indianbartonka.util.network.Network;
@@ -17,24 +21,82 @@ public class NetworkUtilTest {
 
 
     @Test
-    public void testIPv4() {
+    public void testIPv4() throws SocketException {
         for (final Network network : NetworkUtil.getIPv4()) {
-            System.out.println("Nazwa wyświetlana: " + network.networkInterface().getDisplayName());
-            System.out.println("Nazwa: " + network.networkInterface().getName());
-            System.out.println("Address IP: " + network.hostAddress());
+            final NetworkInterface networkInterface = network.networkInterface();
+
+            System.out.println("Nazwa wyświetlana: " + networkInterface.getDisplayName());
+            System.out.println("Nazwa: " + networkInterface.getName());
+            System.out.println("Adres IP: " + network.hostAddress());
+
+            System.out.println("Czy Point-to-Point: " + networkInterface.isPointToPoint());
+            System.out.println("Czy Up: " + networkInterface.isUp());
+            System.out.println("Czy Loopback: " + networkInterface.isLoopback());
+            System.out.println("Czy Virtual: " + networkInterface.isVirtual());
+            System.out.println("Czy multicast: " + networkInterface.supportsMulticast());
+
+            System.out.println();
+            System.out.println("Adresy przypisane do interfejsu");
+            final Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                final InetAddress inetAddress = inetAddresses.nextElement();
+                System.out.println("Adres: " + inetAddress.getHostAddress());
+                System.out.println("Canonical Host Name: " + inetAddress.getCanonicalHostName());
+                System.out.println();
+            }
+
+            final int mtu = networkInterface.getMTU();
+            System.out.println("MTU: " + mtu + " (" + MathUtil.formatBytesDynamic(mtu, false) + ")");
+
+            System.out.println("Grupy multicast");
+            final Enumeration<NetworkInterface> subInterfaces = networkInterface.getSubInterfaces();
+            while (subInterfaces.hasMoreElements()) {
+                System.out.println("Podinterfejs: " + subInterfaces.nextElement().getDisplayName());
+                System.out.println();
+            }
+
+            System.out.println("----------------------------");
             System.out.println();
         }
     }
-
 
     @Test
-    public void testIPv6() {
+    public void testIPv6() throws SocketException {
         for (final Network network : NetworkUtil.getIPv6()) {
-            System.out.println("Nazwa wyświetlana: " + network.networkInterface().getDisplayName());
-            System.out.println("Nazwa: " + network.networkInterface().getName());
-            System.out.println("Address IP: " + network.hostAddress());
+            final NetworkInterface networkInterface = network.networkInterface();
+
+            System.out.println("Nazwa wyświetlana: " + networkInterface.getDisplayName());
+            System.out.println("Nazwa: " + networkInterface.getName());
+            System.out.println("Adres IP: " + network.hostAddress());
+
+            System.out.println("Czy Point-to-Point: " + networkInterface.isPointToPoint());
+            System.out.println("Czy Up: " + networkInterface.isUp());
+            System.out.println("Czy Loopback: " + networkInterface.isLoopback());
+            System.out.println("Czy Virtual: " + networkInterface.isVirtual());
+            System.out.println("Czy multicast: " + networkInterface.supportsMulticast());
+
+            System.out.println();
+            System.out.println("Adresy przypisane do interfejsu");
+            final Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                final InetAddress inetAddress = inetAddresses.nextElement();
+                System.out.println("Adres: " + inetAddress.getHostAddress());
+                System.out.println("Canonical Host Name: " + inetAddress.getCanonicalHostName());
+                System.out.println();
+            }
+
+            final int mtu = networkInterface.getMTU();
+            System.out.println("MTU: " + mtu + " (" + MathUtil.formatBytesDynamic(mtu, false) + ")");
+
+            System.out.println("Grupy multicast");
+            final Enumeration<NetworkInterface> subInterfaces = networkInterface.getSubInterfaces();
+            while (subInterfaces.hasMoreElements()) {
+                System.out.println("Podinterfejs: " + subInterfaces.nextElement().getDisplayName());
+                System.out.println();
+            }
+
+            System.out.println("----------------------------");
             System.out.println();
         }
     }
-
 }
