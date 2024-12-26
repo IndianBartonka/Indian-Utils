f375e8ac4a07de072af82c2836a2f6e23c7dfef9
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-12-26 15:43:16 +0100

    Update UnsupportedSystemException.java

--- src/main/java/pl/indianbartonka/util/exception/UnsupportedSystemException.java
+++ src/main/java/pl/indianbartonka/util/exception/UnsupportedSystemException.java
@@ -5,4 +5,8 @@ public class UnsupportedSystemException extends RuntimeException {
     public UnsupportedSystemException(final String message) {
         super(message);
     }
+
+    public UnsupportedSystemException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
