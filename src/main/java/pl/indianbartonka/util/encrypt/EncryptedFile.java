706f9ef72f159c269ee4c5c5b771dfb877dad937
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2024-12-26 15:53:49 +0100

    Changed from class to record

--- src/main/java/pl/indianbartonka/util/encrypt/EncryptedFile.java
+++ src/main/java/pl/indianbartonka/util/encrypt/EncryptedFile.java
@@ -2,34 +2,6 @@ package pl.indianbartonka.util.encrypt;
 
 import java.io.File;
 
-public class EncryptedFile {
-    //TODO: Rozważ zmienienie tego w Record
+public record EncryptedFile(long encryptTime, long fileSize, File encryptedFile, String encryptionAlgorithm) {
 
-    private final long encryptTime;
-    private final long fileSize;
-    private final File encryptedFile;
-    private final String encryptionAlgorithm;
-
-    public EncryptedFile(final long encryptTime, final long fileSize, final File encryptedFile, final String encryptionAlgorithm) {
-        this.encryptTime = encryptTime;
-        this.fileSize = fileSize;
-        this.encryptedFile = encryptedFile;
-        this.encryptionAlgorithm = encryptionAlgorithm;
-    }
-
-    public long getEncryptTime() {
-        return this.encryptTime;
-    }
-
-    public long getFileSize() {
-        return this.fileSize;
-    }
-
-    public File getEncryptedFile() {
-        return this.encryptedFile;
-    }
-
-    public String getEncryptionAlgorithm() {
-        return this.encryptionAlgorithm;
-    }
 }
