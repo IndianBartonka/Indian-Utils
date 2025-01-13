e9c3faa9bfb8ca14363150f5c0f4bdfb0414314a
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2025-01-13 23:39:53 +0100

    [Phone] add TODO

--- src/main/java/pl/indianbartonka/util/http/UserAgent.java
+++ src/main/java/pl/indianbartonka/util/http/UserAgent.java
@@ -17,30 +17,30 @@ import pl.indianbartonka.util.system.SystemUtil;
 
 @UtilityClass
 public final class UserAgent {
-
+//TODO: Update versions
     /**
      * Predefined User-Agent strings for various tools and browsers
      */
-    public static final String USER_AGENT_CURL = "curl/7.64.1";
-    public static final String USER_AGENT_OKHTTP = "okhttp/3.12.1";
+    public static final String USER_AGENT_CURL = "curl/8.11.1";
+    public static final String USER_AGENT_OKHTTP = "okhttp/5.0.0-alpha.14";
     public static final String USER_AGENT_WINDOWS_POWER_SHELL = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) PowerShell/7.5.0";
 
     /**
      * Browser version strings
      */
-    private static final String CHROME_VERSION = "91.0.4472.114";
-    private static final String FIREFOX_VERSION = "89.0";
-    public static final String USER_AGENT_FIREFOX = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:" + FIREFOX_VERSION + ") Gecko/20100101 Firefox/" + FIREFOX_VERSION;
-    private static final String SAFARI_VERSION = "537.36";
+    private static final String CHROME_VERSION = "110.0.5481.100";
+private static final String FIREFOX_VERSION = "109.0";
+private static final String SAFARI_VERSION = "17.1";
+private static final String EDGE_VERSION = "110.0.1587.49";
+private static final String OPERA_VERSION = "95.0.4635.37";
 
+    
     /**
      * Predefined User-Agent strings for mobile applications
      */
     public static final String USER_AGENT_ANDROID = "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/" + CHROME_VERSION + " Mobile Safari/" + SAFARI_VERSION;
     public static final String USER_AGENT_IOS = "Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/" + SAFARI_VERSION;
-    private static final String EDGE_VERSION = "91.0.864.64";
-    private static final String OPERA_VERSION = "77.0.4054.90";
-
+   
     /**
      * Base User agent
      */
@@ -55,7 +55,8 @@ public final class UserAgent {
     public static final String USER_AGENT_SAFARI = BASE_USER_AGENT + " AppleWebKit/537.36 (KHTML, like Gecko) Version/14.1 Safari/" + SAFARI_VERSION;
     public static final String USER_AGENT_EDGE = BASE_USER_AGENT + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + CHROME_VERSION + " Safari/" + SAFARI_VERSION + " Edg/" + EDGE_VERSION;
     public static final String USER_AGENT_OPERA = BASE_USER_AGENT + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + CHROME_VERSION + " OPR/" + OPERA_VERSION;
-
+ public static final String USER_AGENT_FIREFOX = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:" + FIREFOX_VERSION + ") Gecko/20100101 Firefox/" + FIREFOX_VERSION;
+   
     /**
      * Private constructor to prevent instantiation of the utility class.
      */
