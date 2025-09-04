a3739eb4248595b02ecf5cc0ace8a9263eb0b5ca
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2025-09-04 23:59:12 +0200

    Update Disk.java

--- src/main/java/pl/indianbartonka/util/system/parts/Disk.java
+++ src/main/java/pl/indianbartonka/util/system/parts/Disk.java
@@ -1,6 +1,7 @@
 package pl.indianbartonka.util.system.parts;
 
 import java.io.File;
+import pl.indianbartonka.util.annotation.Since;
 
-public record Disk(String name, String model, File diskFile, String type, long blockSize, boolean readOnly) {
+public record Disk(String name, @Since("0.0.9.5") String model, File diskFile, String type, long blockSize, boolean readOnly) {
 }
